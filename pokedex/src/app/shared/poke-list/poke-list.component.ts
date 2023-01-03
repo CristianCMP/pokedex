65fc16cff1c3399b895d26b1cb90351bb7376a8a
CristianCMP <cristian.puhl@universo.univates.br>
2023-01-02 23:10:11 -0300

    Tela de pesquisa e api

--- pokedex/src/app/shared/poke-list/poke-list.component.ts
+++ pokedex/src/app/shared/poke-list/poke-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PokeApiService } from '../../service/poke-api.service'
+
 @Component({
   selector: 'poke-list',
   templateUrl: './poke-list.component.html',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokeListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private pokeApiService: PokeApiService) { }
 
   ngOnInit(): void {
+    this.pokeApiService.apiListAllPokemons.subscribe(
+      res => res
+    );
   }
 
 }
